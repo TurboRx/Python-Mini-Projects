0319a15420fc352f62be3cc6a26648e23ec7e378
TurboRx <187360786+TurboRx@users.noreply.github.com>
2026-05-07 16:57:49 +0530

    Change membership status to non-member

--- movie_ticket_calculator/ticket_calculator.py
+++ movie_ticket_calculator/ticket_calculator.py
@@ -10,7 +10,7 @@ if age >= 21:
     print('User is eligible for Evening shows')
 else print('User is not eligible for Evening shows')
 
-is_member = True
+is_member = False
 is_weekend = False
 
 discount = 0
