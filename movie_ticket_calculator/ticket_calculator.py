1d04795ae8e7c0f642c60b5cc5a732a70349ed6f
TurboRx <187360786+TurboRx@users.noreply.github.com>
2026-05-08 13:03:09 +0530

    Refactor ticket eligibility and charge calculations (complete it)

--- movie_ticket_calculator/ticket_calculator.py
+++ movie_ticket_calculator/ticket_calculator.py
@@ -8,8 +8,6 @@ if age > 17:
 
 if age >= 21:
     print('User is eligible for Evening shows')
-else:
-    print('User is not eligible for Evening shows')
 
 is_member = False
 is_weekend = False
@@ -26,27 +24,26 @@ print('Discount:', discount)
 
 extra_charges = 0
 
-if is_weekend or show_time == 'Evening':
+if is_weekend:
     extra_charges = 2
     print('Extra charges will be applied')
 else:
     print('No extra charges will be applied')
-    
+
 print('Extra charges:', extra_charges)
 
-if age >= 21:
-    print('Ticket booking condition satisfied')
-else:
-    print('Ticket booking failed due to restrictions')
-    
 if age >= 21 or age >= 18 and (show_time != 'Evening' or is_member):
     print('Ticket booking condition satisfied')
     service_charges = 0
     if seat_type == 'Premium':
         service_charges = 5
-    elif: seat_type == 'Gold':
-          service_charges = 3
-    else: service_charges = 1
+    elif seat_type == 'Gold':
+        service_charges = 3
+    else:
+        service_charges = 1
+    print('Service charges:', service_charges)
+    final_price = base_price + extra_charges + service_charges - discount
+    print('Final price of ticket:', final_price)
 else:
-    print('Service_charges:', service_charges)
-
+    print('Ticket booking failed due to restrictions')
+    
